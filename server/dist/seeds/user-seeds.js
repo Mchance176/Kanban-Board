d101ffe42c39c6323460140c867df2381bd3aeeb
Mchance176 <matt.chance176@gmail.com>
2024-12-03 14:32:07 -0500

    added TS

--- server/dist/seeds/user-seeds.js
+++ server/dist/seeds/user-seeds.js
@@ -1,5 +1,6 @@
 export const seedUsers = async () => {
     try {
+      console.log('Starting to seed users...');
       const users = await User.bulkCreate([
         { username: 'JollyGuru', password: 'password' },
         { username: 'SunnyScribe', password: 'password' },
@@ -7,7 +8,9 @@ export const seedUsers = async () => {
       ], { individualHooks: true });
       
       console.log('Users seeded successfully:', users.map(u => u.username));
+      return users;
     } catch (error) {
       console.error('Error seeding users:', error);
+      throw error;
     }
   };
